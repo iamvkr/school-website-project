7f6097a15c5f629d392c76b01347b7dcddc24814
iamvkr <roysajal@yandex.com>
2024-07-25 21:55:00 +0530

    fixed routing for gh

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,HashRouter  } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 import { UserProvider } from "./context/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <UserProvider>
-  <BrowserRouter>
+  <HashRouter>
     <SnackbarProvider>
       <App />
     </SnackbarProvider>
-  </BrowserRouter>
+  </HashRouter>
   </UserProvider>
 );
